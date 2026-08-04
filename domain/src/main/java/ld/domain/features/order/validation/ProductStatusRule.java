d3beb1c5d13973a3b7d14fb13a5f5fcac0a2d071
Lyes DOUKI <lyes.douki@gmail.com>
2026-08-04 23:27:55 +0200

    refactor prodcut status role

--- domain/src/main/java/ld/domain/features/order/validation/ProductStatusRule.java
+++ domain/src/main/java/ld/domain/features/order/validation/ProductStatusRule.java
@@ -6,21 +6,22 @@ import ld.standard.lib.validation.BusinessRule;
 import ld.standard.lib.validation.Result;
 
 import java.util.List;
+import java.util.UUID;
 
 public class ProductStatusRule implements BusinessRule<CreateOrderContextValidation> {
 
     @Override
     public Result<Void> apply(CreateOrderContextValidation context) {
-        List<String> unavailableProductNames = context.products().stream()
+        List<UUID> unavailableProducts = context.products().stream()
                 .filter(product -> ProductStatus.UNAVAILABLE.equals(product.productStatus()))
-                .map(ProductSnapshot::name)
+                .map(ProductSnapshot::productId)
                 .toList();
 
-        if (!unavailableProductNames.isEmpty()) {
+        if (!unavailableProducts.isEmpty()) {
             return Result.businessFailure(
                     OrderErrorCode.PRODUCT_NOT_AVAILABLE,
                     "Produits indisponibles",
-                    "Les produits suivants sont indisponibles : " + String.join(", ", unavailableProductNames)
+                    String.format("Les produits suivants sont indisponibles (identifiants : %s)", unavailableProducts)
             );
         }
 
