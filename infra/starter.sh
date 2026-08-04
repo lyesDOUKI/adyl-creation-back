4d0bf4302a63ebb64b8a4fe74d7822e87b22a355
Lyes DOUKI <lyes.douki@gmail.com>
2026-08-04 23:19:38 +0200

    chore: refactor starter

--- infra/starter.sh
+++ infra/starter.sh
@@ -19,8 +19,11 @@ docker compose -p postgres-adyl-creation up -d
 cd ../..
 
 echo ""
-echo "3) Starting Backend API..."
+echo "Building new backend image..."
+docker compose build
 
+echo ""
+echo "3/ Starting backend..."
 docker compose up -d
 
 
