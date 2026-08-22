2c8f9322e21fb67c1bd3c3f5caf66b058c2468de
Lyes DOUKI <lyes.douki@gmail.com>
2026-08-22 22:05:19 +0200

    chore: clean not used methods

--- application/src/main/java/ld/application/infra/db/jpa/ProductJpaRepository.java
+++ application/src/main/java/ld/application/infra/db/jpa/ProductJpaRepository.java
@@ -2,18 +2,7 @@ package ld.application.infra.db.jpa;
 
 import ld.application.infra.db.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.Collection;
-import java.util.Set;
 import java.util.UUID;
 
-public interface ProductJpaRepository extends JpaRepository<Product, UUID> {
-
-    @Query("""
-        select p.id
-        from Product p
-        where p.id in :ids
-    """)
-    Set<UUID> findExistingIds(Collection<UUID> ids);
-}
+public interface ProductJpaRepository extends JpaRepository<Product, UUID> {}
