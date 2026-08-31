f9db87f6a4389c840841989029752f69d6ee6955
Lyes DOUKI <lyes.douki@gmail.com>
2026-08-31 11:09:19 +0200

    refactor: better model for order status

--- domain/src/main/java/ld/domain/features/order/model/OrderStatus.java
+++ domain/src/main/java/ld/domain/features/order/model/OrderStatus.java
@@ -6,13 +6,26 @@ import ld.domain.valueObjects.Percentage;
 import java.time.Instant;
 
 public sealed interface OrderStatus {
+
+    String type();
+
     enum State implements OrderStatus {
-        PENDING, REJECTED, DELIVERED
+        PENDING, REJECTED, DELIVERED;
+
+        @Override
+        public String type() {
+            return this.name();
+        }
     }
 
     OrderStatus PENDING = State.PENDING;
     OrderStatus REJECTED = State.REJECTED;
     OrderStatus DELIVERED = State.DELIVERED;
 
-    record Accepted(Instant acceptedAt, Percentage discountApplied) implements OrderStatus {}
+    record Accepted(Instant acceptedAt, Percentage discountApplied) implements OrderStatus {
+        @Override
+        public String type() {
+            return "ACCEPTED";
+        }
+    }
 }
