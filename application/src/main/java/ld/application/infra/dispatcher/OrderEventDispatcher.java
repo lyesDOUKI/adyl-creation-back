595635d86d0147ce3151658a1acceccf33e2f3b7
Lyes DOUKI <lyes.douki@gmail.com>
2026-09-12 00:21:51 +0200

    fix: make spring dispatcher as primary bean for orders

--- application/src/main/java/ld/application/infra/dispatcher/OrderEventDispatcher.java
+++ application/src/main/java/ld/application/infra/dispatcher/OrderEventDispatcher.java
@@ -3,9 +3,11 @@ package ld.application.infra.dispatcher;
 import ld.domain.features.order.model.OrderEvent;
 import ld.standard.lib.AggregateEventDispatcher;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class OrderEventDispatcher implements AggregateEventDispatcher<OrderEvent> {
 
     private final ApplicationEventPublisher applicationEventPublisher;
